b8b45cfe37a3659ba819928ad07edaba650f1c96
instructor <instructor@example>
2018-07-31 18:10:26 +0900

    영민

--- app/src/main/java/bikelong/iot2/goott/bikelong/BikeActivity.java
+++ app/src/main/java/bikelong/iot2/goott/bikelong/BikeActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -69,6 +71,14 @@ public class BikeActivity extends AppCompatActivity {
                         });
                     }
 
+                    //click test
+                    mBikeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Intent intent = new Intent(BikeActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    });
 
                 } catch (Exception e){
                     System.out.println(e);
